db5712ac13b1daa1697db84d10a5f72d002ef74b
sssskm <157735362+sssskm@users.noreply.github.com>
2025-03-24 22:50:51 +0800

    24032025 10502
    added exam assigning components

---
+++ app/Livewire/Exam/Interviewlist.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Exam;
+
+use Livewire\Component;
+
+class Interviewlist extends Component
+{
+    public function render()
+    {
+        return view('livewire.exam.interviewlist');
+    }
+}

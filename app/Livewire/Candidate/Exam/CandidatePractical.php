c2aa0f62ee1d592161efdcfd7f1bd324dd572aab
sssskm <157735362+sssskm@users.noreply.github.com>
2025-03-30 01:18:17 +0800

    30032025 01171
    +added candidate components
    +candidate login
    +candidate home
    +candidate exams

---
+++ app/Livewire/Candidate/Exam/CandidatePractical.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Candidate\Exam;
+
+use Livewire\Component;
+
+class CandidatePractical extends Component
+{
+    public function render()
+    {
+        return view('livewire.candidate.exam.candidate-practical')->layout('components.layouts.candidate-app');
+    }
+}
---
+++ resources/views/livewire/candidate/home.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="d-flex flex-column gap-4">
+            <a href="{{ route('candidate.exam.assessment') }}" class="btn btn-primary btn-lg w-100">Assessment</a>
+            <a href="{{ route('candidate.exam.practical') }}" class="btn btn-success btn-lg w-100">Practical</a>
+            <a href="{{ route('candidate.exam.oral') }}" class="btn btn-danger btn-lg w-100">Oral</a>
+        </div>
+    </div>
+    
+</div>
